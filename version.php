9b7159d0bfee2c73605902275a39de9ad48a1a41
Mark Nielsen <mark@moodlerooms.com>
2018-03-07 12:12:13 -0800

    INT-12974: bump plugin release info

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->version   = 2017120800; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017051500; // Requires this Moodle version.
+$plugin->requires  = 2017111300; // Requires this Moodle version.
 $plugin->component = 'local_redislock'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.3.3';
+$plugin->release   = '3.4.1';
