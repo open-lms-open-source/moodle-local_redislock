c161f97ee438bcfbce204410eb30af35cac07239
Jonathan Garcia <jonathan.garcia@blackboard.com>
2019-11-26 15:31:23 -0500

    INT-15220: Version and required version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
 $plugin->version   = 2018080800; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018120300; // Requires this Moodle version.
+$plugin->requires  = 2019052000; // Requires this Moodle version.
 $plugin->component = 'local_redislock'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.6.3';
+$plugin->release   = '3.7.3';
