101a6fadc793fe24d84c2072241171f3f14f3987
Rafael Monterroza <rafael.monterroza@openlms.net>
2022-07-29 18:07:24 +0000

    INT-18181: bump versions for 3.11 mp4

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2022041300; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2022072900; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version.
 $plugin->component = 'local_redislock'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.11.3';
+$plugin->release   = '3.11.6';
