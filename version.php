977e3be302a83ad288edbc4423b9a0471b567b56
Bryan Cruz <bryan.cruz@openlms.net>
2023-06-15 10:13:23 -0500

    INT-18870: 4.1 Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 /** @var object $plugin */
-$plugin->version   = 2023041100; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041900; // Requires this Moodle version.
+$plugin->version   = 2023061500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2022112800; // Requires this Moodle version.
 $plugin->component = 'local_redislock'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.0.7';
+$plugin->release   = '4.1.3';
