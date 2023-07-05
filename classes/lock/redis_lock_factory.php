d1d811a76c5fc29ebdfa76422a7cbaf2236fee75
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2023-07-05 17:15:14 -0500

    INT-19075: PHPUnit 8.1 fix Passing null to parameter of type X is deprecation

--- classes/lock/redis_lock_factory.php
+++ classes/lock/redis_lock_factory.php
@@ -424,6 +424,6 @@ class redis_lock_factory implements lock_factory {
         return http_build_query(array(
             'hostname' => $this->get_hostname(),
             'processid' => getmypid(),
-        ), null, '&');
+        ), '', '&');
     }
 }
